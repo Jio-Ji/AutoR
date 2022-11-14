360287293ea6366ca1618fc1d483252bd5ee07da
szhou9 <szhou9@ncsu.edu>
2022-11-14 06:49:40 -0500

    Changes by Zaco

---
+++ out/production/AutoR1/dropTable.sql
@@ -0,0 +1,37 @@
+drop table mechanicWeekHour;
+
+drop table invoiceAndService;
+
+drop table invoice;
+
+drop table receptionist;
+
+drop table manager;
+
+drop table timeoff;
+
+drop table timeslot;
+
+drop table mechanic;
+
+drop table emp;
+
+drop table vehicleAndMaintenance;
+
+drop table serviceType;
+
+drop table service;
+
+drop table prices;
+
+drop table associate;
+
+drop table vehicle;
+
+drop table customer;
+
+drop table centerSalary;
+
+drop table center;
+
+drop table user_pw;
